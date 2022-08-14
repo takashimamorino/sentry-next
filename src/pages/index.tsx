ed75ef9a120c24557c66d7a27cb8b8e08f17965e
takashimamorino <does1026@gmail.com>
2022-08-15 05:47:02 +0900

    tmp

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { type NextPage } from 'next';
 import { ErrorButton } from '../components/ErrorButton';
 import { ErrorBoundaryButton } from '../components/ErrorBoundaryButton';
+import { captureMessage } from '@sentry/nextjs';
 
 const HomePage: NextPage = () => {
   return (
@@ -8,8 +9,93 @@ const HomePage: NextPage = () => {
       <h2>Home</h2>
       <ErrorButton />
       <ErrorBoundaryButton />
+      <Fatal />
+      <Error />
+      <Warning />
+      <Info />
+      <Log />
+      <Debug />
     </>
   );
 };
 
 export default HomePage;
+
+
+const Fatal = () => {
+  return (
+    <button
+      type="button"
+      onClick={() => {
+        captureMessage("Fatal", "fatal")
+      }}
+    >
+      fatal
+    </button>
+  )
+}
+
+const Error = () => {
+  return (
+    <button
+      type="button"
+      onClick={() => {
+        captureMessage("Error", "error")
+      }}
+    >
+      error
+    </button>
+  )
+}
+
+const Warning = () => {
+  return (
+    <button
+      type="button"
+      onClick={() => {
+        captureMessage("Warning", "warning")
+      }}
+    >
+      warning
+    </button>
+  )
+}
+
+const Log = () => {
+  return (
+    <button
+      type="button"
+      onClick={() => {
+        captureMessage("Log", "log")
+      }}
+    >
+      log
+    </button>
+  )
+}
+
+const Info = () => {
+  return (
+    <button
+      type="button"
+      onClick={() => {
+        captureMessage("Info", "info")
+      }}
+    >
+      info
+    </button>
+  )
+}
+
+const Debug = () => {
+  return (
+    <button
+      type="button"
+      onClick={() => {
+        captureMessage("Debug", "debug")
+      }}
+    >
+      debug
+    </button>
+  )
+}
